285e54c6bacc877641b0b2db5286dd1eacb74405
wwt <648128145@qq.com>
2018-10-16 15:01:59 +0800

    init

---
+++ components/public/header/geo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="m-geo">
+        <i class="el-icon-location"/>北京市
+        <nuxt-link 
+        class="changeCity" 
+       
+        >切换城市</nuxt-link>
+        [深圳 田径 上海]
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='css'>
+
+</style>
