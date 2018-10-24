cb69eacb2d9c46cb8caaa7dc004ea750145d4a27
wwt <648128145@qq.com>
2018-10-24 16:51:29 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,9 @@
         <e-life></e-life>
       </el-col>
     </el-row>
-    <el-row :span="24">3</el-row>
+    <el-row :span="24">
+      <Artistic></Artistic>
+    </el-row>
   </div>
 </template>
 
@@ -16,10 +18,12 @@
 
 import Emenu from '@/components/index/menu.vue'
 import ELife from '@/components/index/life.vue'
+import Artistic from '@/components/index/artistic.vue'
 export default {
   components: {
     Emenu,
-    ELife
+    ELife,
+    Artistic
   }
 }
 </script>
